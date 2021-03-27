aaa6444a75ce3e16d5866008f0a1302ecbab2a84
Shawn Kim <shawnkim@Shawnui-MacBookPro.local>
2021-03-27 23:12:43 +0900

    Enum

---
+++ EnumEx1/src/Main.java
@@ -0,0 +1,39 @@
+enum Direction { EAST, SOUTH, WEST, NORTH }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Direction d1 = Direction.EAST;
+        Direction d2 = Direction.valueOf("WEST");
+        Direction d3 = Enum.valueOf(Direction.class, "EAST");
+
+        System.out.println("d1 = " + d1);
+        System.out.println("d2 = " + d2);
+        System.out.println("d3 = " + d3);
+
+        System.out.println("d1 == d2 ? " + (d1 == d2));
+        System.out.println("d1 == d3 ? " + (d1 == d3));
+        System.out.println("d1.equals(d3) ? " + d1.equals(d3));
+        System.out.println("d1.compareTo(d3) ? " + d1.compareTo(d3));
+        System.out.println("d1.compareTo(d2) ? " + d1.compareTo(d2));
+
+        switch (d1) {
+            case EAST:
+                System.out.println("The direction is EAST."); break;
+            case SOUTH:
+                System.out.println("The direction is SOUTH."); break;
+            case WEST:
+                System.out.println("The direction is WEST."); break;
+            case NORTH:
+                System.out.println("The direction is NORTH."); break;
+            default:
+                System.out.println("Invalid direction."); break;
+        }
+
+        Direction[] dArr = Direction.values();
+
+        for (Direction d : dArr)
+            System.out.printf("%s = %d%n", d.name(), d.ordinal());
+    }
+}
